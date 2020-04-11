57d673b3d80214070bcfa66d7396e2e588edc9a5
theCCB <cemcembelentepe@gmail.com>
2020-04-11 17:49:12 +0300

    Update emulator.c

--- emulator.c
+++ emulator.c
@@ -20,6 +20,7 @@ int16_t fetch(uint32_t addr)
 //Fetches an instruction from ROM, decodes and executes it
 int32_t execute(void)
 {
+#if 0
     uint32_t pc;
     uint32_t sp;
 	uint32_t inst;
@@ -49,9 +50,48 @@ int32_t execute(void)
 		return(0);
 	}
 	//Rest of the instructions to be implemented here
-
 	fprintf(stderr, "invalid instruction 0x%08X 0x%04X\n", pc - 4, inst);
+#endif
+
+#define R0  (cpu.reg[ 0])
+#define R1  (cpu.reg[ 1])
+#define R2  (cpu.reg[ 2])
+#define R3  (cpu.reg[ 3])
+#define R4  (cpu.reg[ 4])
+#define R5  (cpu.reg[ 5])
+#define R6  (cpu.reg[ 6])
+#define R7  (cpu.reg[ 7])
+#define R8  (cpu.reg[ 8])
+#define R9  (cpu.reg[ 9])
+#define R10 (cpu.reg[10])
+#define R11 (cpu.reg[11])
+#define R12 (cpu.reg[12])
+#define SP  (cpu.reg[13])
+#define LR  (cpu.reg[14])
+#define PC  (cpu.reg[15])
+
+	uint32_t inst = rom[PC - 2] | rom[PC - 1] << 8;
+	PC += 2;
+
+
 	return(1);
+
+#undef R0 
+#undef R1 
+#undef R2 
+#undef R3 
+#undef R4 
+#undef R5 
+#undef R6 
+#undef R7 
+#undef R8 
+#undef R9 
+#undef R10
+#undef R11
+#undef R12
+#undef SP 
+#undef LR 
+#undef PC 
 }
 
 //Resets the CPU and initializes the registers
